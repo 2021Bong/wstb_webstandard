3fe8831e1361cb9268203aed2eac0c35dddd9cdb
Bong <won950705@naver.com>
2022-07-14 10:53:48 +0900

    lecture no.31 with change Jquery into Javascript

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
-'use strict';
+"use strict";
 
-const nav = document.querySelector('.nav');
-const titBtn = document.querySelector('.tit .btn');
+// main menu
+const nav = document.querySelector(".nav");
+const titBtn = document.querySelector(".tit .btn");
 
 function handleTitleMenu() {
-  if (nav.classList.contains('on')) {
-    nav.classList.remove('on');
-    titBtn.classList.remove('show');
+  if (nav.classList.contains("on")) {
+    nav.classList.remove("on");
+    titBtn.classList.remove("show");
   } else {
-    nav.classList.add('on');
-    titBtn.classList.add('show');
+    nav.classList.add("on");
+    titBtn.classList.add("show");
   }
 }
 
-titBtn.addEventListener('click', handleTitleMenu);
+titBtn.addEventListener("click", handleTitleMenu);
+
+//tab menu
+const tabMenu = document.querySelector("#tab_menu > ul");
+const tabLi = document.querySelectorAll("#tab_menu > ul > li");
+
+tabLi.forEach((item) => item.addEventListener("click", tabHandler));
+
+function tabHandler(e) {
+  e.preventDefault();
+  const titleList = e.target.parentNode;
+
+  if (titleList.classList !== "active") {
+    tabLi.forEach((item) => {
+      item.classList.remove("active");
+    });
+    titleList.classList.add("active");
+  }
+}
